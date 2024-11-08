8cbfb9bcfaefa735e46a049067a0a01cf8e538c9
Lucer <104328289+Lucer007@users.noreply.github.com>
2024-11-08 14:16:44 -0500

    First Push

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a public repo that follows CS50 offered by Harvard Uni 
+
+https://pll.harvard.edu/course/cs50-introduction-computer-science
---
+++ welcome.py
@@ -0,0 +1,6 @@
+#Welcome! This is my code for CS 50 from Havard university
+
+def print_hi(name):
+    print(f'Hi, This is {name} \nHave fun following CS50')
+
+print_hi(name='Lucer/Lijith')
